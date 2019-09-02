d6cf69faf9ddcebddd5089ee90a21257b9933256
Travel <1263869057@qq.com>
2019-09-02 12:05:25 +0800

    icons图标

--- Travels/src/page/home/components/Icons.vue
+++ Travels/src/page/home/components/Icons.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="Icons">
-    <div class="icon">
-      <div class="icon-img" v-for="item in iconList" :key="item.id">
-        <img :src="item.imgUrl" alt />
-        <p class="icon-img-font">{{item.dsec}}</p>
-      </div>
-    </div>
+  <div class="icons">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
@@ -14,81 +19,143 @@ export default {
   name: "Icons",
   data() {
     return {
+      swiperOptions: {
+        autopaly: false,
+        pagination: ".swiper-pagination"
+      },
       iconList: [
         {
           id: "0001",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          dsec: "景点门票"
+          desc: "景点门票"
         },
         {
           id: "0002",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png",
-          dsec: "故宫"
+          desc: "故宫"
         },
         {
           id: "0003",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
-          dsec: "北京必游"
+          desc: "北京必游"
         },
         {
           id: "0004",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png",
-          dsec: "水上乐园"
+          desc: "水上乐园"
         },
         {
           id: "0005",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
-          dsec: "一日游"
+          desc: "一日游"
         },
         {
           id: "0006",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png",
-          dsec: "古北水镇"
+          desc: "古北水镇"
         },
         {
           id: "0007",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
-          dsec: "动植物园"
+          desc: "动植物园"
         },
         {
           id: "0008",
           imgUrl:
             "http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png",
-          dsec: "奥林匹克塔"
+          desc: "奥林匹克塔"
+        },
+        {
+          id: "0009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png",
+          desc: "杭州宋城"
+        },
+        {
+          id: "0010",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/12375c435e1bc4a52074f84f40a685a0.png",
+          desc: "杭州极地"
         }
       ]
     };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
   }
 };
 </script>
 
 <style lang='stylus' scoped>
-.Icons
+.icons >>> .swiper-pagination-bullet-active
+  background: rgba(0, 175, 190, 0.8)
+  width: 6px
+  height: 6px
+.icons >>> .swiper-pagination-bullet
+  width: 7px
+  height: 7px
+.icons
+  position: relative
   overflow: hidden
   height: 0
-  padding-bottom: 50%
-  .icon
-    display: flex
-    flex-wrap: wrap
+  padding-bottom: 55%
+  .swiper-container
+    overflow: visible
+  .swiper-wrapper
+    height: 0 !important
+  .swiper-pagination
+    position: absolute
+    bottom: -0.4rem
+    padding: 0 6px
     width: 100%
-    padding-bottom: 25%
+    text-align: center
+  .swiper-icon
+    width: 100%
+  .icon
+    position: relative
     overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
     .icon-img
-      text-align: center
-      padding: 2.5% 0
-      width: 25%
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0.44rem
       box-sizing: border-box
-      img
-        width: 50%
-      .icon-img-font
-        font-size: 0.25rem
-        height: 0.44rem
-        line-height: 0.44rem
+      padding-top: 0.2rem
+      .icon-img-content
+        width: 1.1rem
+        height: 1.1rem
+        margin: 0 auto
+        display: block
+    .icon-desc
+      position: absolute
+      line-height: 0.44rem
+      height: 0.44rem
+      left: 0
+      right: 0
+      bottom: 0
+      font-size: 0.2rem
+      color: #333
+      text-align: center
 </style>
