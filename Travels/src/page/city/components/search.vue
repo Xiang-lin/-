6f75531fe77800f7aa3f15fd7c4f8dd6a9557cca
Travel <1263869057@qq.com>
2019-09-10 09:35:48 +0800

    input-search

---
+++ Travels/src/page/city/components/search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "City-search"
+};
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.search
+  height: 0.62rem
+  padding: 0 0.1rem
+  background: $bgColor
+  .search-input
+    box-sizing: border-box
+    padding: 0 0.1rem
+    height: 0.52rem
+    line-height: 0.52rem
+    width: 100%
+    text-align: center
+    border-radius: 0.06rem
+    outline: none
+    color: #666
+</style>
